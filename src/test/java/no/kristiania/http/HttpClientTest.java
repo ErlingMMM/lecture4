f621519e028c66a95d76ad578f8c7c2b64f5b45a
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2021-09-19 19:13:48 +0200

    test shouldReturnStatusCode (fails)

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -5,8 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
+
     @Test
     void shouldDoSomething(){
         assertEquals(200, 100+100);
+    }
+
+    @Test
+    void shouldReturnStatusCode(){
+        assertEquals(200, new HttpClient("httpbin.org",80,"/html").getStatusCode());
+
+
+
     }
 }
